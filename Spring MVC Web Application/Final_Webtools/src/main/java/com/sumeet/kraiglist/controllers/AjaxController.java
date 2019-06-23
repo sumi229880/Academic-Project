17d3993352d2b4b53a352e278b2e1165fbcd7080
shwetharam4 <sahanaram4@gmail.com>
2019-06-23 09:51:33 +0530

    Offline Commits

--- Spring MVC Web Application/Final_Webtools/src/main/java/com/sumeet/kraiglist/controllers/AjaxController.java
+++ Spring MVC Web Application/Final_Webtools/src/main/java/com/sumeet/kraiglist/controllers/AjaxController.java
@@ -18,98 +18,79 @@ public class AjaxController {
 
 	@Autowired
 	UserDAO userDao;
-	
-	@RequestMapping(value="/searchuser.htm" , method = RequestMethod.POST)
-    protected String doSubmitActionUser(HttpServletRequest request,HttpServletResponse response)
-        throws Exception
-    {
-		try
-		{
+
+	@RequestMapping(value = "/searchuser.htm", method = RequestMethod.POST)
+	protected String doSubmitActionUser(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		try {
 			String username = request.getParameter("user");
 			System.out.println("username" + username);
 			User usersearch;
-        	usersearch = userDao.get(username);
-        	 JSONObject obj = new JSONObject();
-             obj.put("users",usersearch.getUsername());
-             PrintWriter out = response.getWriter();
-             out.println(obj);
-        	
-    }
-        catch(Exception e)
-        {
-            System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
-        }
-        return null;
-    }
-	@RequestMapping(value="/searchmail.htm" , method = RequestMethod.POST)
-    protected String doSubmitActionMail(HttpServletRequest request,HttpServletResponse response)
-        throws Exception
-    {
-		try
-		{
+			usersearch = userDao.get(username);
+			JSONObject obj = new JSONObject();
+			obj.put("users", usersearch.getUsername());
+			PrintWriter out = response.getWriter();
+			out.println(obj);
+
+		} catch (Exception e) {
+			System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
+		}
+		return null;
+	}
+
+	@RequestMapping(value = "/searchmail.htm", method = RequestMethod.POST)
+	protected String doSubmitActionMail(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		try {
 			String username = request.getParameter("user");
 			System.out.println("username" + username);
 			User usersearch;
-        	usersearch = userDao.getmail(username);
-        	 JSONObject obj = new JSONObject();
-             obj.put("users",usersearch.getUsername());
-             PrintWriter out = response.getWriter();
-             out.println(obj);
-        	
-    }
-        catch(Exception e)
-        {
-            System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
-        }
-        return null;
-    }
-	
-	@RequestMapping(value="/searchhusky.htm" , method = RequestMethod.POST)
-    protected String doSubmitActionHusky(HttpServletRequest request,HttpServletResponse response)
-        throws Exception
-    {
-		try
-		{
+			usersearch = userDao.getmail(username);
+			JSONObject obj = new JSONObject();
+			obj.put("users", usersearch.getUsername());
+			PrintWriter out = response.getWriter();
+			out.println(obj);
+
+		} catch (Exception e) {
+			System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
+		}
+		return null;
+	}
+
+	@RequestMapping(value = "/searchhusky.htm", method = RequestMethod.POST)
+	protected String doSubmitActionHusky(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		try {
 			String username = request.getParameter("user");
 			System.out.println("usernameisisisisis" + username);
 			User usersearch;
-        	usersearch = userDao.gethusky(username);
-        	 JSONObject obj = new JSONObject();
-             obj.put("users",usersearch.getUsername());
-             PrintWriter out = response.getWriter();
-             out.println(obj);
-        	
-    }
-        catch(Exception e)
-        {
-            System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
-        }
-        return null;
-    }
-	
-	@RequestMapping(value="/searchLogin.htm" , method = RequestMethod.POST)
-    protected String doSubmitActionLogin(HttpServletRequest request,HttpServletResponse response)
-        throws Exception
-    {
-		try
-		{
+			usersearch = userDao.gethusky(username);
+			JSONObject obj = new JSONObject();
+			obj.put("users", usersearch.getUsername());
+			PrintWriter out = response.getWriter();
+			out.println(obj);
+
+		} catch (Exception e) {
+			System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
+		}
+		return null;
+	}
+
+	@RequestMapping(value = "/searchLogin.htm", method = RequestMethod.POST)
+	protected String doSubmitActionLogin(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		try {
 			String username = request.getParameter("user");
 			String password = request.getParameter("pass");
 			System.out.println("username" + username);
 			System.out.println("pass" + password);
 			User usersearch;
-        	usersearch = userDao.loginValidation(username,password);
-        	 JSONObject obj = new JSONObject();
-             obj.put("users",usersearch);
-             PrintWriter out = response.getWriter();
-             out.println(obj);
-        	
-    }
-        catch(Exception e)
-        {
-            System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
-        }
-        return null;
-    }
- 
+			usersearch = userDao.loginValidation(username, password);
+			JSONObject obj = new JSONObject();
+			obj.put("users", usersearch);
+			PrintWriter out = response.getWriter();
+			out.println(obj);
+
+		} catch (Exception e) {
+			System.out.println((new StringBuilder("Exception: ")).append(e.getMessage()).toString());
+		}
+		return null;
+	}
+
 }
